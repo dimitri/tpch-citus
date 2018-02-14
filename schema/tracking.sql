b9344aa95fb5200c6912069f0ad64eb6ad1f2eb7
Dimitri Fontaine <dim@tapoueh.org>
2018-02-14 20:57:32 +0100

    Add a Queries Per Minute view to the tracking database.

--- schema/tracking.sql
+++ schema/tracking.sql
@@ -42,6 +42,7 @@ create or replace view results
             run.system as system,
             run.schedule as schedule,
             job.name as job,
+            row_number() over(partition by run.id order by job.id) as job_number,
             job.start,
             job.duration,
             case when job.steps is not null
@@ -56,5 +57,38 @@ create or replace view results
             left join query on query.job = job.id
    group by run.id, job.id
    order by run.id, job.start;
+
+
+create or replace view qpm as
+     select system,
+            job.name,
+
+            case when job.steps is not null
+                 then job.steps[array_upper(job.steps, 1)]
+             end as sf,
+
+            job.duration,
+
+            case when job.steps is null
+                 then avg(query.duration)
+             end as qtime,
+
+            count(query.id) as queries,
+
+            round((  count(query.id)
+                   / extract(epoch from sum(query.duration))
+                   * 60)::numeric,
+                  4) as qpm
+
+       from job
+                 join run on run.id = job.run
+            left join query on query.job = job.id
+
+      where job.steps is not null
+         or job.name ~ 'user-stream'
+
+     group by run.id, job.id, query.job
+     order by run.id, job.start;
+
 
 commit;
